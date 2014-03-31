372b76b7c18f4917de21902ccb5ffc436607373b
Mark <me@jsman.ru>
2014-03-31 22:37:48 +0300

    DrawTriangle

--- main.go
+++ main.go
@@ -8,13 +8,13 @@ import (
 )
 
 func main() {
-	var a = CreateRaster(10, 10)
+	var a = CreateRaster(50, 10)
 	a.SetColor(Color{0xFF, 0xFF, 0xFF})
 	//a.DrawPoint(1, 1)
 	//a.DrawPoint(5, 5)
 	a.SetColor(Color{0xAA, 0xAA, 0xAA})
 	//a.DrawPoint(0, 0)
 	// fmt.Printf("%#v", a)
-	a.DrawLine(1, 1, 5, 5)
+	a.DrawTriangle(25, 0, 49, 3, 0, 9)
 	debug.PrintRaster(a)
 }
--- raster/raster.go
+++ raster/raster.go
@@ -2,7 +2,6 @@ package raster
 
 import (
 	. "github.com/jsmarkus/goart/color"
-	//"math"
 )
 
 type Raster struct {
@@ -27,7 +26,7 @@ func (r *Raster) GetColorAt(x, y uint32) Color {
 }
 
 func (r *Raster) xyToOffset(x, y uint32) uint32 {
-	return x + r.Height*y
+	return x + r.Width*y
 }
 
 func (r *Raster) DrawPoint(x, y uint32) {
@@ -58,9 +57,7 @@ func (r *Raster) DrawLine(x1, y1, x2, y2 uint32) {
 	var signY int8 = sign(int32(y2 - y1))
 	var error int32 = deltaX - deltaY
 	r.DrawPoint(x2, y2)
-	_ = signX
-	_ = signY
-	_ = error
+
 	for x1 != x2 || y1 != y2 {
 		r.DrawPoint(x1, y1)
 		var error2 int32 = error * 2
@@ -72,5 +69,59 @@ func (r *Raster) DrawLine(x1, y1, x2, y2 uint32) {
 			error += deltaX
 			y1 += uint32(signY)
 		}
+	}
+}
+
+func (r *Raster) DrawTriangle(x1, y1, x2, y2, x3, y3 uint32) {
+	////http://www.gamedev.ru/code/forum/?id=95311
+	if y1 > y2 {
+		y1, y2 = y2, y1
+		x1, x2 = x2, x1
+	}
+	if y2 > y3 {
+		y3, y2 = y2, y3
+		x3, x2 = x2, x3
+	}
+	if y1 > y2 {
+		y1, y2 = y2, y1
+		x1, x2 = x2, x1
+	}
+
+	if y1 == y2 && y2 == y3 {
+		return
+	}
+
+	var xb, xe float32
+
+	//TODO: use constants
+	//TODO: convert to integer algorithm
+
+	fy1 := float32(y1)
+	fy2 := float32(y2)
+	fy3 := float32(y3)
+	fx1 := float32(x1)
+	fx2 := float32(x2)
+	fx3 := float32(x3)
+
+	for scanline := fy1; scanline < fy2; scanline++ {
+		xb = fx2 + (scanline-fy2)*(fx1-fx2)/(fy1-fy2)
+		xe = fx3 + (scanline-fy3)*(fx1-fx3)/(fy1-fy3)
+
+		if xb > xe {
+			xb, xe = xe, xb
+		}
+
+		r.DrawLine(uint32(xb), uint32(scanline), uint32(xe), uint32(scanline))
+	}
+
+	for scanline := fy2; scanline < fy3; scanline++ {
+		xb = fx3 + (scanline-fy3)*(fx2-fx3)/(fy2-fy3)
+		xe = fx3 + (scanline-fy3)*(fx1-fx3)/(fy1-fy3)
+
+		if xb > xe {
+			xb, xe = xe, xb
+		}
+
+		r.DrawLine(uint32(xb), uint32(scanline), uint32(xe), uint32(scanline))
 	}
 }
