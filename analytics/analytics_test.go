896652089fd9e8a5d408d8e4a42f99330030fb62
Mark <me@jsman.ru>
2014-04-04 17:10:53 +0300

    WIP symmetry

--- analytics/analytics_test.go
+++ analytics/analytics_test.go
@@ -5,7 +5,34 @@ import (
 	"testing"
 )
 
-func TestSymmetry(t *testing.T) {
+func TestAsymmetry(t *testing.T) {
 	rst := graphics.CreateRaster(4, 4)
-	Symmetry(rst)
+	rst.SetColor(graphics.Color{5, 60, 71})
+	if Asymmetry(rst) > 0 {
+		t.Error("assymetry is wrong")
+	}
+	rst.DrawPoint(2, 1)
+	if Asymmetry(rst) <= 0 {
+		t.Error("assymetry is wrong")
+	}
+	rst.DrawPoint(1, 1)
+	if Asymmetry(rst) > 0 {
+		t.Error("assymetry is wrong")
+	}
+}
+
+func TestColorDistance(t *testing.T) {
+	if ColorDistance(
+		&graphics.Color{15, 15, 15},
+		&graphics.Color{15, 15, 15},
+	) != 0 {
+		t.Error("color distance is wrong")
+	}
+
+	if ColorDistance(
+		&graphics.Color{15, 15, 15},
+		&graphics.Color{15, 15, 16},
+	) <= 0 {
+		t.Error("color distance is wrong")
+	}
 }
