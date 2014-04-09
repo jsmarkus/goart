6b2d98b51505a16867a4e4e38d09bdb55766e6ea
Mark <me@jsman.ru>
2014-04-09 16:17:32 +0300

    improved triangle renderer

--- app/triangle_renderer.go
+++ app/triangle_renderer.go
@@ -9,38 +9,36 @@ import (
 const geneChainLen = 9 //color: 3; coords: 3*2
 
 type TriangleRenderer struct {
-	Genome *ga.GenomeInt
 	Raster *graphics.Raster
 }
 
-func NewTriangleRenderer(genome *ga.GenomeInt, raster *graphics.Raster) *TriangleRenderer {
+func NewTriangleRenderer(raster *graphics.Raster) *TriangleRenderer {
 	return &TriangleRenderer{
-		Genome: genome,
 		Raster: raster,
 	}
 }
 
-func (this *TriangleRenderer) Render() {
+func (this *TriangleRenderer) Render(genome *ga.GenomeInt) {
 	this.Raster.Clear()
 	var colorDivider = uint32(ga.GenomeIntMaxValue) / 255
 	var xDivider = uint32(ga.GenomeIntMaxValue) / this.Raster.Width
 	var yDivider = uint32(ga.GenomeIntMaxValue) / this.Raster.Height
-	for i := uint32(0); i < this.Genome.Len(); i += geneChainLen {
+	for i := uint32(0); i < genome.Len(); i += geneChainLen {
 		offset := i
 		color := graphics.Color{
-			R: uint8(this.Genome.Gene[offset] / colorDivider),
-			G: uint8(this.Genome.Gene[offset+1] / colorDivider),
-			B: uint8(this.Genome.Gene[offset+2] / colorDivider),
+			R: uint8(genome.Gene[offset] / colorDivider),
+			G: uint8(genome.Gene[offset+1] / colorDivider),
+			B: uint8(genome.Gene[offset+2] / colorDivider),
 		}
 		//fmt.Println(color)
-		x1 := this.Genome.Gene[offset+3] / xDivider
-		y1 := this.Genome.Gene[offset+4] / yDivider
+		x1 := genome.Gene[offset+3] / xDivider
+		y1 := genome.Gene[offset+4] / yDivider
 
-		x2 := this.Genome.Gene[offset+5] / xDivider
-		y2 := this.Genome.Gene[offset+6] / yDivider
+		x2 := genome.Gene[offset+5] / xDivider
+		y2 := genome.Gene[offset+6] / yDivider
 
-		x3 := this.Genome.Gene[offset+7] / xDivider
-		y3 := this.Genome.Gene[offset+8] / yDivider
+		x3 := genome.Gene[offset+7] / xDivider
+		y3 := genome.Gene[offset+8] / yDivider
 
 		//fmt.Printf("(%d,%d) (%d,%d) (%d,%d)\n", x1, y1, x2, y2, x3, y3)
 		this.Raster.SetColor(color)
--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 	"github.com/jsmarkus/goart/graphics"
 )
 
-const NG = 1000000
+const NG = 100000
 
-func main() {
+func experiment(pipe chan string) {
 	var generation [NG]*ga.GenomeInt
 	for i := 0; i < NG; i++ {
 		genome := ga.NewGenomeInt(5 * 9)
@@ -18,15 +18,27 @@ func main() {
 		generation[i] = genome
 	}
 
+	raster := graphics.CreateRaster(10, 10)
+	renderer := app.NewTriangleRenderer(raster)
+
 	for i := 0; i < NG; i++ {
-		renderer := app.NewTriangleRenderer(
-			generation[i],
-			graphics.CreateRaster(10, 10))
-		renderer.Render()
-		as := analytics.Asymmetry(renderer.Raster)
-		if as <= 600 {
-			fmt.Printf("%d: as=%d\n", i, as)
-			graphics.PrintRaster(renderer.Raster)
+		renderer.Render(generation[i])
+		as := analytics.Asymmetry(raster)
+		if as <= 1000 {
+			pipe <- fmt.Sprintf("%d: as=%d\n", i, as)
+			//fmt.Printf("%d: as=%d\n", i, as)
+			//graphics.PrintRaster(raster)
 		}
+	}
+
+	close(pipe)
+
+}
+
+func main() {
+	pipe := make(chan string)
+	go experiment(pipe)
+	for msg := range pipe {
+		fmt.Println(msg)
 	}
 }
