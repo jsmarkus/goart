fed3b2d85e0cf3eb14216948026934eb6a5aaa37
Mark <me@jsman.ru>
2014-04-10 16:57:02 +0300

    removed unused interface

--- ga/generation.go
+++ ga/generation.go
@@ -4,31 +4,28 @@ import (
 	"math/rand"
 )
 
-type Generation interface {
-}
-
-type GenerationS struct {
+type Generation struct {
 	Genome []Genome
 }
 
-func NewGeneration(length uint32, genomeLength uint32, constr GenomeConstructor) *GenerationS {
+func NewGeneration(length uint32, genomeLength uint32, constr GenomeConstructor) *Generation {
 	gnm := make([]Genome, length)
 	for i := uint32(0); i < length; i++ {
 		gnm[i] = constr(genomeLength)
 	}
-	return &GenerationS{
+	return &Generation{
 		Genome: gnm,
 	}
 }
 
-func (this *GenerationS) Mutate(strength, geneProbability, genomeProbability float32) {
+func (this *Generation) Mutate(strength, geneProbability, genomeProbability float32) {
 	for _, gnm := range this.Genome {
 		if rand.Float32() < genomeProbability {
 			gnm.Mutate(strength, geneProbability)
 		}
 	}
 }
-func (this *GenerationS) Randomize() {
+func (this *Generation) Randomize() {
 	for _, gnm := range this.Genome {
 		gnm.Randomize()
 	}
